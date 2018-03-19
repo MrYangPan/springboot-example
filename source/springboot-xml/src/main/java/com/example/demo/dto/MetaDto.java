29371b54a01d6f08b97e5c70655b14062a2808e6
Mr.PanYang <1280043826@qq.com>
2018-03-19 10:46:30 +0800

    file

---
+++ source/springboot-xml/src/main/java/com/example/demo/dto/MetaDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.MetaVo;
+
+public class MetaDto extends MetaVo {
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
